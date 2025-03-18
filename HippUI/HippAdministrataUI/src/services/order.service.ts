331be77e0acfcde1a8286423d034b834b6b35374
melossta <statovcimelos96@gmail.com>
2025-03-18 01:25:58 +0100

    Added order request modal to the manager. Added the ability to the client to request an update or delete an order.

--- HippUI/HippAdministrataUI/src/services/order.service.ts
+++ HippUI/HippAdministrataUI/src/services/order.service.ts
@@ -31,4 +31,36 @@ export class OrderService {
 
     return this.http.post('https://localhost:7136/api/Order/request', requestData);
   }
+  requestUpdateOrder(requestData: {
+    orderId: number;
+    clientId: number;
+    requestType: string;
+    reason: string;
+    newDeliveryDestination?: string;
+    newQuantity?: number;
+    newProductId?: number;
+  }): Observable<any> {
+    return this.http.post('https://localhost:7136/api/Order/request', requestData);
+  }
+  
+
+
+
+  updateOrder(orderId: number, updatedOrder: any): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('authToken')}`);
+    // Updated URL with the 'update' path
+    return this.http.put(`${this.apiUrl}/Order/update/${orderId}`, updatedOrder, { headers });
+  }
+
+  updateOrderRequest(body: any): Observable<any> {
+    return this.http.post(this.apiUrl, body, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
+  
+  
+  
+  
 }
